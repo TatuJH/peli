4ebb886836dd5c82157741eac7b74c54d9472d1b
TatuJH <tatu.helminen@metropolia.fi>
2025-09-19 13:02:31 +0300

    Initial commit

---
+++ python/event_list.py
@@ -0,0 +1,11 @@
+events = {
+    1: "You are given an investment opportunity on the street by a man in a trench coat. He says that by giving him $100 you could make $300."
+}
+
+choices = {
+    1: "Do you accept (a) or decline (d)?"
+}
+
+results = {
+    1: ["The opportunity actually paid off. You earned $300.", "The opportunity was real... But you lost $100.", "It was a scam! You chase the man and manage to get your money back. You also earn $100 extra, but lose 5 days in the process."]
+}
