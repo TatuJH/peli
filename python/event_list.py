ff88e40ad273ceaf04f6674bb30c52c9aaee0684
TatuJH <tatu.helminen@metropolia.fi>
2025-09-19 13:02:47 +0300

    initial

--- python/event_list.py
+++ python/event_list.py
@@ -1,11 +1,40 @@
 events = {
-    1: "You are given an investment opportunity on the street by a man in a trench coat. He says that by giving him $100 you could make $300."
-}
-
-choices = {
-    1: "Do you accept (a) or decline (d)?"
-}
-
-results = {
-    1: ["The opportunity actually paid off. You earned $300.", "The opportunity was real... But you lost $100.", "It was a scam! You chase the man and manage to get your money back. You also earn $100 extra, but lose 5 days in the process."]
+    1:{
+        "event":"You are given an investment opportunity on the street by a man in a trench coat. He says that by giving him \033[32m$100\033[0m you could make \033[32m$300\033[0m." ,
+        "input":"Do you accept (a) or decline (d)?",
+        "choices":{
+            "a":{
+                "cost":{"money":100,"time":0,"artefacts":0},
+                "results":{
+                    1:{"money":300,"time":0,"artefacts":0,"text":"The opportunity actually paid out. You earned \033[32m$300.\033[0m"},
+                    2:{"money":0,"time":0,"artefacts":0,"text":"The opportunity was real... But you lost your \033[32m$100\033[0m."},
+                    3:{"money":200,"time":-5,"artefacts":0,"text":"The man takes off running! You chase him and get your \033[32m$100\033[0m back. You also manage to snatch \033[32m$100\033[0m extra from him, but lose \033[34m5 days\033[0m in the process."}
+                }
+            },
+            "d":{
+                "cost":{"money":0,"time":0,"artefacts":0},
+                "results":{
+                    1:{"money":0,"time":0,"artefacts":0,"text":"You decline the man's offer. It was probably a scam anyway."}
+                }
+            }
+        }
+    },
+    2:{
+        "event":"You meet a witch, who says she can buy you time. Literally. She offers you \033[34m30 days\033[0m in exchange for \033[32m$100\033[0m.",
+        "input":"Do you accept (a) or decline (d)?",
+        "choices":{
+            "a":{
+                "cost":{"money":100,"time":0,"artefacts":0},
+                "results":{
+                    1:{"money":0,"time":30,"artefacts":0,"text":"You bought yourself some time with \033[32m$100\033[0m. \033[34m30 days\033[0m to be exact."}
+                }
+            },
+            "d":{
+                "cost":{"money":0,"time":0,"artefacts":0},
+                "results":{
+                    1:{"money":0,"time":-5,"artefacts":0,"text":"You decline the witch's offer. She gets mad and curses you, draining you of \033[34m5 days\033[0m."}
+                }
+            }
+        }
+    }
 }
---
+++ python/main.py
@@ -0,0 +1,47 @@
+import random
+from event_list import *
+
+money = 1000
+time = 365
+artefacts = 0
+
+
+def event():
+    global money
+    global time
+    global artefacts
+    event_id = random.randint(1, len(events))
+    print(events[event_id]["event"])
+
+    choice = ""
+    while choice not in events[event_id]["choices"] or money < events[event_id]["choices"][choice]["cost"][
+        "money"] or time < events[event_id]["choices"][choice]["cost"]["time"] or artefacts < events[event_id]["choices"][choice]["cost"]["artefacts"]:
+        choice = input(events[event_id]["input"]).strip().lower()
+        if choice in events[event_id]["choices"]:
+            if money < events[event_id]["choices"][choice]["cost"]["money"] and time < events[event_id]["choices"][choice]["cost"]["time"] and artefacts < events[event_id]["choices"][choice]["cost"]["artefacts"]:
+                print("Before acting on it, you realize that you don't have enough of anything for this option.")
+            elif money < events[event_id]["choices"][choice]["cost"]["money"] and time < events[event_id]["choices"][choice]["cost"]["time"]:
+                print("Before acting on it, you realize that you don't have enough money nor time for this option.")
+            elif money < events[event_id]["choices"][choice]["cost"]["artefacts"] and artefacts < events[event_id]["choices"][choice]["cost"]["artefacts"]:
+                print("Before acting on it, you realize that you don't have enough money nor artefacts for this option.")
+            elif time < events[event_id]["choices"][choice]["cost"]["time"] and artefacts < events[event_id]["choices"][choice]["cost"]["artefacts"]:
+                print("Before acting on it, you realize that you don't have enough time nor artefacts for this option.")
+            elif money < events[event_id]["choices"][choice]["cost"]["money"]:
+                print("Before acting on it, you realize that you don't have enough money for this option.")
+            elif time < events[event_id]["choices"][choice]["cost"]["time"]:
+                print("Before acting on it, you realize that you don't have enough time for this option.")
+            elif artefacts < events[event_id]["choices"][choice]["cost"]["artefacts"]:
+                print("Before acting on it, you realize that you don't have enough artefacts for this option.")
+    money -= events[event_id]["choices"][choice]["cost"]["money"]
+    time -= events[event_id]["choices"][choice]["cost"]["time"]
+    artefacts -= events[event_id]["choices"][choice]["cost"]["artefacts"]
+    outcome = random.randint(1, len(events[event_id]["choices"][choice]["results"]))
+    print(events[event_id]["choices"][choice]["results"][outcome]["text"])
+    money += events[event_id]["choices"][choice]["results"][outcome]["money"]
+    time += events[event_id]["choices"][choice]["results"][outcome]["time"]
+    artefacts += events[event_id]["choices"][choice]["results"][outcome]["artefacts"]
+
+event()
+print(money)
+print(time)
+print(artefacts)
