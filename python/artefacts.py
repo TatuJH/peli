641b9c47a303d0a3a898ea0136a4818ab846af03
funkykong1 <vilho.valkeala@gmail.com>
2025-09-19 14:52:06 +0300

    artefakti inventory

---
+++ python/artefacts.py
@@ -0,0 +1,25 @@
+# Sanakirja, jossa jokainen mantereen lyhennys vastaa useaa artifaktin nimeä
+artefact_names = \
+    {
+# Europe
+"EU" : ("Ivory Figurine", "Celtic Sword"),
+
+# Oceania
+"OC" : ("Boomerang", "Didgeridoo"),
+
+# Africa
+"AF" : ("Ancient Spearhead", "Cool rock"),
+
+# South America
+"SA" : ("Tobacco Pipe", "Aztec Sword"),
+
+# North America
+"NA" : ("Flint Arrowhead", "Feathered Headdress"),
+
+# Asia
+"AS" : ("Jade Sculpture", "Bronze Vessel"),
+
+#
+"AN" : ("Carved Bone", "Nondescript Remains")
+
+}
--- python/main.py
+++ python/main.py
@@ -1,15 +1,36 @@
 import random
 from event_list import *
+from artefacts import *
 
 money = 1000
 time = 365
-artefacts = 0
+artefacts = list()
 
-class Auto:
+# testausta varten
+cont = "EU"
+
+
+
+class Artefact:
     def __init__(self, nimi, arvo, manner):
         self.nimi = nimi
         self.arvo = arvo
         self.manner = manner
+
+def add_artefact(count):
+    global cont
+    # satunnainen raha-arvo
+    val = random.randint(600,1000)
+
+    tup = artefact_names[cont]
+
+    # Hanki nimi mantereen perusteella toisesta tiedostosta
+    for i in range(0,count):
+        nimi = tup[random.randint(0, len(tup) - 1)]
+        artefacts.append(Artefact(nimi, val, cont))
+
+def remove_artefact(count):
+    i = random.randint(0, len(artefacts)-1)
 
 
 def event():
@@ -21,38 +42,42 @@ def event():
 
     choice = ""
     while choice not in events[event_id]["choices"] or money < events[event_id]["choices"][choice]["cost"][
-        "money"] or time < events[event_id]["choices"][choice]["cost"]["time"] or artefacts < events[event_id]["choices"][choice]["cost"]["artefacts"]:
+        "money"] or time < events[event_id]["choices"][choice]["cost"]["time"] or len(artefacts) < events[event_id]["choices"][choice]["cost"]["artefacts"]:
         choice = input(events[event_id]["input"]).strip().lower()
 
         if choice in events[event_id]["choices"]:
-            if money < events[event_id]["choices"][choice]["cost"]["money"] and time < events[event_id]["choices"][choice]["cost"]["time"] and artefacts < events[event_id]["choices"][choice]["cost"]["artefacts"]:
+            if money < events[event_id]["choices"][choice]["cost"]["money"] and time < events[event_id]["choices"][choice]["cost"]["time"] and len(artefacts) < events[event_id]["choices"][choice]["cost"]["artefacts"]:
                 print("Before acting on it, you realize that you don't have enough of anything for this option.")
             elif money < events[event_id]["choices"][choice]["cost"]["money"] and time < events[event_id]["choices"][choice]["cost"]["time"]:
                 print("Before acting on it, you realize that you don't have enough money nor time for this option.")
-            elif money < events[event_id]["choices"][choice]["cost"]["artefacts"] and artefacts < events[event_id]["choices"][choice]["cost"]["artefacts"]:
+            elif money < events[event_id]["choices"][choice]["cost"]["artefacts"] and len(artefacts) < events[event_id]["choices"][choice]["cost"]["artefacts"]:
                 print("Before acting on it, you realize that you don't have enough money nor artefacts for this option.")
-            elif time < events[event_id]["choices"][choice]["cost"]["time"] and artefacts < events[event_id]["choices"][choice]["cost"]["artefacts"]:
+            elif time < events[event_id]["choices"][choice]["cost"]["time"] and len(artefacts) < events[event_id]["choices"][choice]["cost"]["artefacts"]:
                 print("Before acting on it, you realize that you don't have enough time nor artefacts for this option.")
             elif money < events[event_id]["choices"][choice]["cost"]["money"]:
                 print("Before acting on it, you realize that you don't have enough money for this option.")
             elif time < events[event_id]["choices"][choice]["cost"]["time"]:
                 print("Before acting on it, you realize that you don't have enough time for this option.")
-            elif artefacts < events[event_id]["choices"][choice]["cost"]["artefacts"]:
+            elif len(artefacts) < events[event_id]["choices"][choice]["cost"]["artefacts"]:
                 print("Before acting on it, you realize that you don't have enough artefacts for this option.")
 
     money -= events[event_id]["choices"][choice]["cost"]["money"]
     time -= events[event_id]["choices"][choice]["cost"]["time"]
-    artefacts -= events[event_id]["choices"][choice]["cost"]["artefacts"]
+
+    if events[event_id]["choices"][choice]["cost"]["artefacts"] > 0:
+        remove_artefact(events[event_id]["choices"][choice]["cost"]["artefacts"])
     outcome = random.randint(1, len(events[event_id]["choices"][choice]["results"]))
     print(events[event_id]["choices"][choice]["results"][outcome]["text"])
     money += events[event_id]["choices"][choice]["results"][outcome]["money"]
     time += events[event_id]["choices"][choice]["results"][outcome]["time"]
-    artefacts += events[event_id]["choices"][choice]["results"][outcome]["artefacts"]
+    #artefacts += events[event_id]["choices"][choice]["results"][outcome]["artefacts"]
+    add_artefact(events[event_id]["choices"][choice]["results"][outcome]["artefacts"])
 
 event()
+add_artefact(1)
 print(money)
 print(time)
-print(artefacts)
+print(artefacts[0].nimi, artefacts[0].arvo, artefacts[0].manner)
 
 
 
