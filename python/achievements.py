03dfc5146085cdbe5eace14f547543532b723ae2
funkykong1 <vilho.valkeala@gmail.com>
2025-10-07 11:37:41 +0300

    achivment

--- python/achievements.py
+++ python/achievements.py
@@ -1,17 +1,21 @@
 achievements = {
     "distance":[
-        #(5000,"\033[1mFirst Steps\033[0m for completing your first flight",0),
-        (30000,"\033[1mBeginner Traveler\033[0m for travelling \033[36m30000 km\033[0m",50),
+        (10000,"\033[1mFirst Steps\033[0m for travelling \033[36m10000 km\033[0m",50),
+        (30000,"\033[1mBeginner Traveler\033[0m for travelling \033[36m30000 km\033[0m",75),
         (60000,"\033[1mIntermediate Traveler\033[0m for travelling \033[36m60000 km\033[0m",100),
-        (100000,"\033[1mAdvanced Traveler\033[0m for travelling \033[36m100000 km\033[0m",150),
-        (200000,"\033[1mMaster Traveler\033[0m for travelling .\033[36m200000 km\033[0m",200),
+        (80000,"\033[1mAdvanced Traveler\033[0m for travelling \033[36m80000 km\033[0m",150),
+        (125000,"\033[1mMaster Traveler\033[0m for travelling \033[36m125000 km\033[0m",300),
+        (175000,"\033[1mBusiness Class\033[0m for travelling \033[175000 km\033[0m",400),
+        (200000,"\033[1mFirst Class\033[0m for travelling \033[36m250000 km\033[0m",500),
+        (250000,"\033[1mApostle\033[0m for travelling \033[36m250000 km\033[0m",500),
         (9999999999999999999999,"error",999999999999)
     ],
     "countries":[
-        #(3,"\033[1mSightseer\033[0m for visiting 3 countries",50),
+        (3,"\033[1mSightseer\033[0m for visiting 4 countries",75),
         (6,"\033[1mTourist\033[0m for visiting 6 countries",100),
         (10,"\033[1mRegular\033[0m for visiting 10 countries",150),
-        (16,"\033[1mDual Citizenship\033[0m for visiting 16 countries",200),
+        (13,"\033[1mDual Citizenship\033[0m for visiting 13 countries",200),
+        (18, "\033[1mMissionary\033[0m for visiting 18 countries", 350),
         (9999999999999999999999,"error",9999999999)
     ],
     "money":[
@@ -29,8 +33,8 @@ achievements = {
         (9999999999999999999999,"error",9999999999999)
     ],
     "events":[
-        (2,"\033[1mRisk-taker\033[0m for completing 2 events",50),
-        (5,"\033[1mLucky Guy\033[0m for completing 5 events",100),
+        (3,"\033[1mRisk-taker\033[0m for completing 2 events",75),
+        (6,"\033[1mLucky Guy\033[0m for completing 5 events",125),
         (10,"\033[1mTrue Adventurer\033[0m for completing 10 events",150),
         (16,"\033[1mFortuna\033[0m for completing 16 events",200),
         (999999999999999999999,"error",9999999999999)
@@ -39,7 +43,8 @@ achievements = {
         (1,"\033[1mBeliever\033[0m for converting heretics 1 time",50),
         (2,"\033[1mFaithful\033[0m for converting heretics 2 times",100),
         (4,"\033[1mDevotee\033[0m for converting heretics 4 times",150),
-        (7,"\033[1mChosen One\033[0m for converting heretic 7 times",200),
+        (7,"\033[1mChosen One\033[0m for converting heretics 7 times",200),
+        (7,"\033[1mMandate From Heaven\033[0m for converting heretics 10 times",350),
         (999999999999999999999,"error",99999999)
     ]
 }
