a0cd1988e84184887fe8afb4cd92015d29d6a186
Leo <leo.niittymaa@metropolia.fi>
2025-12-01 14:21:18 +0200

    jotain pienta

--- html/java/script1.js
+++ html/java/script1.js
@@ -1,27 +1,34 @@
-const instructions = document.querySelector('#instructions');
-if (instructions) {
-    instructions.addEventListener('click', function(evt){
+const instructions_button = document.querySelector('#instructions_button');
+if (instructions_button) {
+    instructions_button.addEventListener('click', function(evt){
     location.href = "instructions.html";
 });
 }
 
-const scores = document.querySelector('#scores');
-if (scores) {
-    scores.addEventListener('click', function(evt){
+const scores_button = document.querySelector('#scores_button');
+if (scores_button) {
+    scores_button.addEventListener('click', function(evt){
     location.href = "scores.html";
 });
 }
 
-const start = document.querySelector('#start');
-if (start) {
-    start.addEventListener('click', function(evt){
+const start_button = document.querySelector('#start_button');
+if (start_button) {
+    start_button.addEventListener('click', function(evt){
     location.href = "main.html";
 });
 }
 
-const new_game = document.querySelector('#new_game');
-if (new_game) {
-    new_game.addEventListener('click', function(evt){
+const new_game_button = document.querySelector('#new_game_button');
+if (new_game_button) {
+    new_game_button.addEventListener('click', function(evt){
+    location.href = "intro.html";
+});
+}
+
+const backpack_button = document.querySelector('#backpack_button');
+if (backpack_button) {
+    backpack_button.addEventListener('click', function(evt){
     location.href = "intro.html";
 });
 }
